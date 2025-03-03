c3a52ed6cceb9774a2d6b8446540c880fab8c8fd
Tamagotchi <marryskiss132@gmail.com>
2025-03-04 00:59:01 +0400

    restarting the game

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -3,19 +3,22 @@ using TMPro;
 using Coin.Coins;
 using FlappyBird.Core;
 using VContainer;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
     [SerializeField] private GameObject _gameOverPanel;
     [SerializeField] private PlayerContactsPoint _contactsPoint;
     [SerializeField] private TextMeshProUGUI _scoreText;
+    [SerializeField] private Button _restartButton;
 
     private WalletData _coins;
 
     [Inject]
     public void Construct(GameState gameState)
     {
-        _coins = gameState.Wallet;   
+        _coins = gameState.Wallet;
+        _restartButton.onClick.AddListener(GameOver);
     }
 
     public void Initialize(PlayerContactsPoint contactsPoint)
